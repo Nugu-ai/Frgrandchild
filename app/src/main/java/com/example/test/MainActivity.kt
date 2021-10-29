56d1bebc11283d88967ef06bba38d4e3d3fc9ed4
이름 <>
2021-10-29 14:37:00 +0900

    Update Activity and fragment

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.test
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var tabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,21 +19,46 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         configureBottomNavigation()
+        tabLayout = findViewById(R.id.tl_ac_main_bottom_menu)
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab!!.position){
+                    0 -> highbar.text = "홈"
+                    1 -> highbar.text = "친절한 설명서"
+                    2 -> highbar.text = "동영상 보기"
+                    3 -> highbar.text = "내 공간"
+
+                }
+            }
+        })
+        setting.setOnClickListener{
+            val nextIntent = Intent(this, SettingActivity::class.java)
+            startActivity(nextIntent)
+        }
+        fab.setOnClickListener{
+            val nextIntent = Intent(this, FloatingActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+
+
     }
     private fun configureBottomNavigation(){
 
         vp_ac_main_frag_pager.adapter = MainFragmentStatePagerAdapter(supportFragmentManager, 4)
-
         tl_ac_main_bottom_menu.setupWithViewPager(vp_ac_main_frag_pager)
-
         val bottomNaviLayout: View = this.layoutInflater.inflate(R.layout.bottom_navigation_tab, null, false)
-
         tl_ac_main_bottom_menu.getTabAt(0)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_home_tab) as RelativeLayout
         tl_ac_main_bottom_menu.getTabAt(1)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_guide_tab) as RelativeLayout
         tl_ac_main_bottom_menu.getTabAt(2)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_video_tab) as RelativeLayout
         tl_ac_main_bottom_menu.getTabAt(3)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_user_tab) as RelativeLayout
-
-
-
     }
+
+
 }
